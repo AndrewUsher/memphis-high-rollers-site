81ea950a99122784d44b270a8932b46c2015adc7
Andrew Usher <andrewusher00@gmail.com>
2022-05-18 14:12:24 -0500

    init

---
+++ app/routes/index.tsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  return (
+    <div className="w-screen h-screen bg-black flex items-center justify-center bg-gradient-to-r from-rose-300 via-yellow-500 to-purple-400">
+      <h1
+        className="text-white text-4xl font-bold"
+        style={{ textShadow: '2px 4px 3px rgba(0,0,0,0.1)' }}
+      >
+        Coming Soon
+      </h1>
+    </div>
+  )
+}
