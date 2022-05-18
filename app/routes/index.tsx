95e7c425c7201f88b5a4e57431bc7c462012e8a3
Andrew Usher <andrewusher00@gmail.com>
2022-05-18 14:17:59 -0500

    fix styling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,14 @@
+import type { MetaFunction } from '@remix-run/node'
+
+export const meta: MetaFunction = () => ({
+  title: 'Memphis High Rollers'
+})
+
 export default function Index() {
   return (
-    <div className="w-screen h-screen bg-black flex items-center justify-center bg-gradient-to-r from-rose-300 via-yellow-500 to-purple-400">
+    <div className="w-screen h-screen bg-black flex items-center justify-center bg-gradient-to-br from-rose-300 via-yellow-500 to-purple-400">
       <h1
-        className="text-white text-4xl font-bold"
+        className="text-white text-4xl font-bold mt-[-50px]"
         style={{ textShadow: '2px 4px 3px rgba(0,0,0,0.1)' }}
       >
         Coming Soon
